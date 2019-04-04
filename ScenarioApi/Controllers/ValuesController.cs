191c423cd815d2973c20fa683007b7af1f9fc961
holtjonathan <holtjonathan@gmail.com>
2019-04-04 01:00:17 -0400

    Testing CI/CD

--- ScenarioApi/Controllers/ValuesController.cs
+++ ScenarioApi/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace ScenarioApi.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "Did", "I", "Change?" };
         }
 
         // GET api/values/5
